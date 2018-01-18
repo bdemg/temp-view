065d7a1f79dca324ff87003264ffd420d5c20a32
Jorge Cano <jacano.sosa@gmail.com>
2018-01-18 09:26:36 -0600

    Docstrings añadidas para todas las clases vista y funciones en el views principal

--- temp_registry/views.py
+++ temp_registry/views.py
@@ -14,14 +14,24 @@ from temp_registry.models import TemperatureSensor, TemperatureReadout, Room, Bu
 
 
 class SensorRegistration(View):
+    """Clase vista que se encarga de manejar las peticiones hechas a la url /register_sensor/"""
+
     template_name = "temp_registry/sensor_registration.html"
     existing_error = "Ya existe un sensor con esa dirección MAC"
 
     def get(self, request):
+        """Esta función se llama cuando el se hace un GET a la url de /register_sensor/. La función
+         se encarga de desplegar la página donde se puede hacer el registro de los sensores por
+         medio de un formulario."""
+
         context = {"form": SensorForm()}
         return render(request, self.template_name, context)
 
     def post(self, request):
+        """Esta función se llama cuando el se hace un POST a la url de /register_sensor/. La función
+            se encarga de leer los datos ingresados en el formulario de registro de sensor y, si no
+            se tiene uno registrado con la misma MAC, se guarda la información en la base de datos."""
+
         form = SensorForm(request.POST)
 
         if form.is_valid() and not TemperatureSensor.objects.filter(
@@ -42,6 +52,8 @@ class SensorRegistration(View):
 
 
 class SensorUpdate(View):
+    """Clase vista que se encarga de manejar las peticiones hechas a la url /update_sensor/mac=<MAC_del_sensor>/"""
+
     template_name = "temp_registry/sensor_update.html"
 
     def get_id_or_empty_string(self, value):
@@ -51,6 +63,9 @@ class SensorUpdate(View):
             ""
 
     def get(self, request, MAC_address):
+        """Esta función se llama cuando el se hace un GET a la url de /update_sensor/mac=<MAC_del_sensor>/.
+            La función se encarga de obtener los datos del sensor indicado y desplegar la página donde
+            se puede modificar los datos relativos a este por medio de un formulario."""
 
         if TemperatureSensor.objects.filter(MAC_address=MAC_address).exists():
 
@@ -66,6 +81,10 @@ class SensorUpdate(View):
         return render(request, self.template_name, context)
 
     def post(self, request, MAC_address):
+        """Esta función se llama cuando el se hace un POST a la url de /update_sensor/mac=<MAC_del_sensor>/.
+            La función se encarga de verificar si existe el sensor correspondiente a la MAC recibida para
+            luego actualizar los datos de este con la información recibida por medio del formulario de
+            actualización."""
 
         instance = get_object_or_404(TemperatureSensor, MAC_address=MAC_address)
         form = SensorForm(request.POST or None, instance=instance)
@@ -85,32 +104,55 @@ class SensorUpdate(View):
 
 
 class GeneralPage(View):
+    """Clase vista que se encarga de manejar las peticiones hechas a la url /general/"""
+
     template_name = "temp_registry/general_view.html"
 
     def get(self, request):
+        """Esta función se llama cuando el se hace un GET a la url de /general/.
+            La función se encarga de desplegar la pagina inicial del sitio web."""
+
         context = {}
         return render(request, self.template_name, context)
 
 
 class RoomAndBuildingRegistrationPage(View):
+    """Clase vista que se encarga de manejar las peticiones hechas a la url /room_building_registration/"""
+
     template_name = "temp_registry/building_and_room_registry.html"
 
     def get(self, request):
+        """Esta función se llama cuando el se hace un GET a la url de /room_building_registration/.
+            La función se encarga de desplegar la pagina donde se pueden registrar nuevos cuartos
+            y edificios."""
+
         context = {"room_form": RoomForm(), "building_form": BuildingForm()}
         return render(request, self.template_name, context)
 
 
 class RoomAndBuildingDeletePage(View):
+    """Clase vista que se encarga de manejar las peticiones hechas a la url /room_building_delete/"""
+
     template_name = "temp_registry/building_and_room_delete.html"
 
     def get(self, request):
+        """Esta función se llama cuando el se hace un GET a la url de /room_building_delete/.
+            La función se encarga de desplegar la pagina donde se puede elegir un cuarto o
+            edificio para eliminar."""
+
         context = {"rooms": Room.objects.all(), "buildings": Building.objects.all()}
         return render(request, self.template_name, context)
 
 
 class ReportsPage(View):
+    """Clase vista que se encarga de manejar las peticiones hechas a la url /reports/"""
+
     template_name = "temp_registry/general_reports.html"
 
     def get(self, request):
+        """Esta función se llama cuando el se hace un GET a la url de /reports/. La función se encarga
+            de desplegar la pagina donde se puede ingresar el sensor, período y fecha para generar un
+            reporte con las lecturas del sensor."""
+
         context = {"sensors": TemperatureSensor.objects.all()}
         return render(request, self.template_name, context)
